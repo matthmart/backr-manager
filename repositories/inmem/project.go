1dca080f30064e13693d6a8d84d5ab8134628b8c
Matthieu Martin <syfonseq@gmail.com>
2019-07-19 15:15:18 +0200

    clean inmem project repo

--- repositories/inmem/project.go
+++ repositories/inmem/project.go
@@ -1,58 +1,23 @@
 package inmem
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 	"time"
 
 	"github.com/agence-webup/backr/manager"
 )
 
-// var sharedRepo projectRepo
-
 // NewProjectRepository returns an instance of
 // an in-memory Project Repository
 func NewProjectRepository() manager.ProjectRepository {
-	sharedRepo := projectRepo{
-		// filepath: "db.json",
+	r := projectRepo{
 		projectsByName: map[string]manager.Project{},
 	}
-
-	// sharedRepo.projectsByName = sharedRepo.readFromFile()
-
-	return &sharedRepo
+	return &r
 }
 
 type projectRepo struct {
-	filepath       string
 	projectsByName map[string]manager.Project
-}
-
-func (repo *projectRepo) readFromFile() map[string]manager.Project {
-	projectsByName := map[string]manager.Project{}
-	if _, err := os.Stat(repo.filepath); os.IsNotExist(err) {
-		return projectsByName
-	}
-	f, err := os.OpenFile(repo.filepath, os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		log.Fatal("project repo: unable to read file:", err)
-	}
-	err = json.NewDecoder(f).Decode(&projectsByName)
-	if err != nil {
-		log.Fatal("project repo: unable to unmarshal data:", err)
-	}
-	return projectsByName
-}
-
-func (repo *projectRepo) saveToFile() {
-	j, err := json.Marshal(repo.projectsByName)
-	if err != nil {
-		log.Fatal("project repo: unable to save to file:", err)
-	}
-	ioutil.WriteFile(repo.filepath, j, os.ModePerm)
 }
 
 func (repo *projectRepo) GetAll() ([]manager.Project, error) {
@@ -79,6 +44,6 @@ func (repo *projectRepo) Save(project manager.Project) error {
 	}
 
 	repo.projectsByName[project.Name] = project
-	// repo.saveToFile()
+
 	return nil
 }
